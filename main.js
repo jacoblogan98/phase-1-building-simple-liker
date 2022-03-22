c54273bf6168a402600bd28bc3d3a947baaee997
Jacob Logan <jacoblogan98@gmail.com>
2022-03-22 15:11:27 -0400

    commit tree 793969c9252cbe94355b01d9d59bda3b80c32790

--- main.js
+++ main.js
@@ -3,8 +3,37 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+document.addEventListener("DOMContentLoaded", () => {
+
+const errorModal = document.querySelector("#modal");
+const likeBttns = document.querySelectorAll("span.like-glyph");
+
+likeBttns.forEach(button => button.addEventListener("click", fetchSC));
+
+function fetchSC(button) {
+  mimicServerCall()
+  .then(() => {
+    if (button.target.textContent === EMPTY_HEART) {
+      button.target.textContent = FULL_HEART
+      button.target.classList.add("activated-heart")
+    }
+  else if (button.target.textContent === FULL_HEART) {
+    button.target.textContent = EMPTY_HEART
+    button.target.classList.remove("activated-heart")
+  }
+})
+  .catch(() => {
+    errorModal.classList.remove("hidden")
+
+    setTimeout(() => {
+      errorModal.className = "hidden"}, 3000)
+  });
+} 
+})
 
 
+
+  
 
 
 //------------------------------------------------------------------------------
